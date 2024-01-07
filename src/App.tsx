74928d138dee98f22d076c6101082c0ab595a10a
muizece <muiz.ece@gmail.com>
2024-01-07 23:13:52 +0530

    Added Navbar and login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './Components/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import { About } from './Components/About/About';
+import { Home } from './Components/Home/Home';
+import { Services } from './Components/Services/Services';
+import { Contact } from './Components/Contact/Contact';
+import {Dashboard} from './Components/Dashboard/Dashboard'
+import { Login } from './Components/Login/Login';
 
 function App() {
   return (
     <div >
-    
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/> 
+        <Route path='/about' element={<About/>}/> 
+        <Route path='/services' element={<Services/>}/> 
+        <Route path='/contact' element={<Contact/>}/> 
+        <Route path='/dashboard' element={<Dashboard/>}/> 
+        <Route path='/login' element={<Login/>}/> 
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/NavBar/MenuData.jsx
@@ -0,0 +1,26 @@
+export const MenuData =[
+    {
+        title : "Home",
+        url:"/",
+        cName:"nav-links",
+        icon: "fas fa-home"
+    },
+    {
+        title : "About",
+        url:"about",
+        cName:"nav-links",
+        icon: "fas fa-info-circle"
+    },
+    {
+        title : "Dashboard",
+        url:"dashboard",
+        cName:"nav-links",
+        icon: "fas fa-briefcase"
+    },
+     {
+        title : "Contact",
+        url:"contact",
+        cName:"nav-links",
+        icon: "fas fa-address-book"
+    }
+]
